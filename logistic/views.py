afe03f50637558927e596617c5b89b89961bb03d
Oleg-Gavrilyuk <gavrilyuk161090@yandex.ru>
2022-12-16 20:52:05 +0300

    s

---
+++ logistic/views.py
@@ -0,0 +1,29 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
+from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.viewsets import ModelViewSet
+
+from logistic.models import Product, Stock
+from logistic.serializers import ProductSerializer, StockSerializer
+
+
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    filter_backends = [SearchFilter]
+    search_fields = ['title', 'description', ]
+    pagination_class = LimitOffsetPagination
+
+
+class StockViewSet(ModelViewSet):
+    queryset = Stock.objects.all()
+    serializer_class = StockSerializer
+    filter_backends = [DjangoFilterBackend]
+    pagination_class = LimitOffsetPagination
+
+    def get_queryset(self):
+        queryset = Stock.objects.all()
+        product = self.request.query_params.get('products')
+        if product is not None:
+            queryset = queryset.filter(product__id=product)
+        return queryset
